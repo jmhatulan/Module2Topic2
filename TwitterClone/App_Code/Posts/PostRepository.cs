8730c62bf1a11d69f04288558150596372f9bc6e
jmhatulan <joshuapyguelhatulan@gmail.com>
2024-03-04 21:22:12 +0800

    Add project files.

---
+++ TwitterClone/App_Code/Posts/PostRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+namespace TwitterClone.App_Code.Posts
+{
+    public class PostRepository
+    {
+        //public IEnumerable<Post> GetAllPosts()
+        //{
+
+
+        //        return new List<Post>()
+        //    {
+        //        new Post(){ Content = "Hello World", PostedBy = "joblipat"},
+        //        new Post(){ Content = "Hello new World", PostedBy = "joelee" },
+        //        new Post(){ Content = "Hello new new World" , PostedBy = "jimlim"},
+        //        new Post(){ Content = "Hi World" , PostedBy = "joblipat"}
+        //   };
+        //}
+        public IEnumerable<Post> GetAllPosts()
+        {
+            var connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\IT114L-B54-22\source\repos\TwitterClone\TwitterClone\App_Data\TwitterClone.mdf;Integrated Security=True";
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = connection.CreateCommand())
+            {
+                connection.Open();
+                command.CommandText =
+                    "SELECT * " +
+                    "FROM Posts " +
+                    "ORDER BY postedOn DESC";
+                return command
+                    .ExecuteReader()
+                    .Cast<IDataRecord>()
+                    .Select(row => new Post()
+                    {
+                        Content = (string)row["content"],
+                        PostedBy = (string)row["postedBy"]
+                    })
+                    .ToList();
+            }
+        }
+    }
+}
---
+++ TwitterClone/Default.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TwitterClone.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head runat="server">
+
+    <title></title>
+
+</head>
+
+<body>
+
+    <form id="form1" runat="server">
+        <div>
+            <%--<asp:Repeater ID="PostRepeater" runat="server">
+                <ItemTemplate>
+                    <p> <%# Eval("Content") %> </p>
+                </ItemTemplate>
+            </asp:Repeater>--%>
+
+            <%--<% if (currentPost.PostedBy == "joblipat") %>
+            <% { %>
+                <p><%= currentPost.Content %></p>
+                <p><%= currentPost.PostedBy %></p>
+            <% } %>--%>
+
+            <%foreach (var currentPost in posts)%>
+            <% { %>
+            <% if (currentPost.PostedBy == "joblipat" || currentPost.PostedBy == "joelim") %>
+                <% { %>
+                    <p><%= currentPost.Content %></p>
+                    <p><%= currentPost.PostedBy %></p>
+                <% } %>
+            <% } %>
+
+
+        </div>
+    </form>
+
+</body>
+
+</html>
+
+<%-- 
+    Repeaters "or" <% for %>
+--%>
